299af951f1d19d69c20b5f3b5a12613f1320a0bc
kang121209 <127708228+kang121209@users.noreply.github.com>
2023-04-25 15:41:18 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -9,4 +9,4 @@ export const defaultSetting = {
 export const defaultMessage = `
 - 由 [OpenAI API (gpt-3.5-turbo)] 提供支持。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
--  即日起，请使用:https://next.gaodun.com,使用企业微信扫码登录后，继续使用AI相关功能和服务。
+- 即日起，请使用:https://next.gaodun.com,使用企业微信扫码登录后，继续使用AI相关功能和服务。
